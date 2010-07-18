d1003ba5ebf055d526c325bfd0778decef6b0ded
Rune Jacobsen <havremunken@gmail.com>
2010-07-18 23:37:02 +0200

    Add GetReleaseVersion project

---
+++ GetReleaseVersion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ReleaseHandlerLibrary;
+
+namespace GetReleaseVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Need release type name to check for");
+                Environment.ExitCode = 1;
+                return;
+            }
+
+            var versionFile = new VersionFile();
+            versionFile.Load("Version.ini");
+
+            var ver = versionFile.GetVersionByName(args[0]);
+            if (ver == null)
+                Console.WriteLine("");
+            else
+                Console.WriteLine(ver.ToString());
+            return;
+
+        }
+    }
+}

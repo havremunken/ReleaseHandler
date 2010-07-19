73cc58eff5edb2cecf51aea27d9efa78554648cc
Rune Jacobsen <havremunken@gmail.com>
2010-07-19 11:08:20 +0200

    Releasing alpha v0.0.0.1

--- ReleaseHandlerLibrary.Tests/AssemblyInfo.cs
+++ ReleaseHandlerLibrary.Tests/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1.5")]
-[assembly: AssemblyFileVersion("0.1.1.5")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
